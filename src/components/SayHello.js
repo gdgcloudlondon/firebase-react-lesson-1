a8b85bd5f3b826d5e79a9de94085813934273e3c
Duncan Ogle <duncan.ogle@gmail.com>
2019-11-04 13:39:28 +0000

    Create const definition

--- src/components/SayHello.js
+++ src/components/SayHello.js
@@ -11,12 +11,14 @@ import React from 'react';
  *  import SayHello from './SayHello';
  */
 const SayHello = (props) => {
+  const personToSayHelloTo = props.to;
+
   return (
     <div className="card">
       <img src="https://picsum.photos/300/200" className="card-img-top" alt="..." />
       <div className="card-body">
         <h5 className="card-title">
-          Hello, {props.to}!
+          Hello, {personToSayHelloTo}!
         </h5>
         <p className="card-text">Welcome to the course.</p>
       </div>
